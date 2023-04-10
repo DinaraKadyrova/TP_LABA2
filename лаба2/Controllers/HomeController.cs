8f899f123fb579aa4c365734da2988d1e99e7a13
DinaraKadyrova <99093818+DinaraKadyrova@users.noreply.github.com>
2023-04-10 09:36:19 +0400

    Добавьте файлы проекта.

---
+++ лаба2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using лаба2.Models;
+
+namespace лаба2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string content = @"<form method='post'>
+                <label>Name:</label><br />
+                <input name='name' /><br />
+                <label>Age:</label><br />
+                <input type='number' name='age' /><br />
+                <input type='submit' value='Send' />
+            </form>";
+            Response.ContentType = "text/html;charset=utf-8";
+            //await Response.WriteAsync(content);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(dataset User)
+        {
+            return View(User);
+        }
+        public IActionResult details()
+        {
+            return View(User);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ лаба2/Models/dataset.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace лаба2.Models
+{
+    [DisplayName("новый пациент")]
+    public class dataset
+    {
+        public string? id { get; set; }
+
+        [DisplayName("Имя")]
+        public string? name { get; set; }
+
+        [DisplayName("Фамилия")]
+        public string? lastname { get; set; }
+
+        [DisplayName("Дата рождения")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? data { get; set; }
+
+        [DisplayName("Номер телефона")]
+        [DataType(DataType.PhoneNumber)]
+        public uint number { get; set; }
+
+        [DisplayName("Адрес")]
+        public string? text { get; set; }
+
+        [DisplayName("Пол")]
+        public bool sex { get; set; }
+    }
+}
